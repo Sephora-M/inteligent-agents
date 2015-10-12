72913dd1b6a086f9c7228a5c927c506020b7bc29
Sephora Madjiheurem <Sephora@tsf-452-wpa-5-194.epfl.ch>
2015-10-12 03:58:37 +0200

    extremely messy for now allow a few more hours for a cleaned up and non buggy version

--- reactive/src/template/State.java
+++ reactive/src/template/State.java
@@ -9,23 +9,38 @@ public class State {
 	public static enum ActionType {PICK, MOVE}
 	private final City mCurrentCity;
 	private final City mDestinationCity;
-	private List<ActionContainer> mActions;
+	private double value;
+	private ActionStruct bestAction;
+//	private List<ActionStruct> actions;
 	
+	public ActionStruct getBestAction() {
+		return bestAction;
+	}
+
+	public void setBestAction(ActionStruct bestAction) {
+		this.bestAction = bestAction;
+	}
+
 	public State(City currentCity, City destinationCity) {
 		mCurrentCity = currentCity;
 		mDestinationCity = destinationCity;
+		value = 0.0;
 	}
 	
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+
 	public City getSourceCity() {
 		return mCurrentCity;
 	}
 	
 	public City getDestinationCity() {
 		return mDestinationCity;
-	}
-	
-	public List<ActionContainer> getPossibleAction() {
-		return mActions;
 	}
 	
 	@Override
@@ -44,4 +59,5 @@ public class State {
 			return mCurrentCity.equals(s.mCurrentCity) && mDestinationCity.equals(s.mDestinationCity);
 		}
 	}
+
 }
